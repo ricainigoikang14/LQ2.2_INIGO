05179bdcd8ab11a6fbbaa321b6532170a307c88d
ricainigoikang14 <inigorica5@gmail.com>
2025-05-19 21:51:49 +0800

    Create Activity2.js

---
+++ Activity2.js
@@ -0,0 +1,6 @@
+let dog_breed prompt("Please enter the enter the dog breed");
+let printCount =parseInt(prompt("How many times would the dog breed be print?"));
+
+for(let i=0; i < printCount; i++){
+   console.log(dog_breed);
+}
